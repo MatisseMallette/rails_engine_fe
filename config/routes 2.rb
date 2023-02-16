eafee094a74ce718f1c470ebb6cf977c29ec1e1d
Matisse Mallette <matissemallette@gmail.com>
2023-02-16 07:39:35 -0700

    Complete bare minimum item index/show functionality

--- config/routes 2.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'merchants/index'
-    end
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/merchants', to: 'api/v1/merchants#index'
-end
